a44b88c4046fb0dcd41b095f6fc1e6b7833c86e2
saepulloh20 <dayat.chaos20@gmail.com>
2022-07-09 09:11:39 +0700

    validation form

--- app/Models/Camp.php
+++ app/Models/Camp.php
@@ -2,12 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Checkout;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Camp extends Model
 {
     use HasFactory, SoftDeletes;
     protected $fillable = ['title', 'price'];
+
+    public function getIsRegisteredAttribute()
+    {
+        if (!Auth::check()) {
+            return false;
+        }
+        return Checkout::whereCampId($this->id)->whereUserId(Auth::id())->exists();
+    }
 }
